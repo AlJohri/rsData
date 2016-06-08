7eb4b28818a1cfdd970453ccdd85a7b6610100f3
cherrywbtc <jielei.wang.0316@gmail.com>
2016-06-08 17:58:23 -0400

    database added

--- dataBase/DBCreator.py
+++ dataBase/DBCreator.py
@@ -8,9 +8,36 @@ from optparse import OptionParser
 # table name to schema mapping
 Schemas= {   
 'houses':
-'''
-''',
+	    '''
+	    address text primary key,
+            town text, 
+            state text,
+            status text,
+            style text,
+            category text,
+            lot text,
+            floor text,
+            rooms text,
+            bedrooms text,
+            bathrooms text,
+            basement text,
+            garage text,
+            heatcool text,
+            yearbuilt text''',
 
+'housemls':
+	    '''
+	    houseID int primary key,
+            mls text''',
+'mlshis':
+	    '''
+            mls text primary key,
+	    listDate date,
+	    soldDate date,
+	    listPrice int,
+	    soldPrice int,
+	    status text
+	    '''
 }
 
 class SqlTable(object):
@@ -21,7 +48,7 @@ class SqlTable(object):
 
     
     def create( self, cursor):
-        pass
+        cursor.execute('''create table '''+self.table_name+''' ('''+self.schema+''')''')
 
 
 def main():
@@ -38,7 +65,11 @@ def main():
     with sql.connect( dbname ) as conn:
         for table_name, schema in Schemas.items():
             SqlTable( table_name, schema ).create(conn)
-
+    conn.execute('''INSERT INTO housemls (houseID, mls) VALUES(559, '482048');''')
+    cursor = conn.execute("SELECT houseID, mls from housemls")
+    for row in cursor:
+    	print "houseID = ", row[0]
+    	print "mls = ", row[1], "\n"
     
 
 main()
